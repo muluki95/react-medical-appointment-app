2fdbeb5c4dc1f191b74de49a0ab3d2128707078d
muluki95 <mulukiesther@gmail.com>
2023-10-16 17:06:34 -0500

    commit updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import LandingPage from "./Components/Landing_Page/LandingPage";
+import Signup from "./Components/Signup/Signup";
+import Login from "./Components/Login/Login";
+
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
           <Navbar/>
               <Routes>
                 <Route path="/" element={<LandingPage/>}/>
+                <Route path="/signup" element={<Signup />}/>
+                <Route path="/login" element={<Login />}/>
+                
               </Routes>
             
         </BrowserRouter>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,43 +2,23 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import './Navbar.css';
 
+
 const Navbar = () => {
 
     
   return (
-    <div>
-<div className="nav_title">
-          <Link href="/">
-          StayHealthy Inc 
-          </Link>
-          <span>.</span>
-        </div>
-        <div className="nav__icon" >
-          <i className="fa fa-times fa fa-bars"></i>
-        </div>
-        <ul className="nav__links active">
-          <li className="link">
-          
-            <Link href="../Landing_Page/LandingPage.html">Home</Link>
-          </li>
-          <li className="link">
-        
-            <Link href="#">Appointments</Link>
-          </li>
-              <li className="link">
-                
-                <Link href="../Sign_Up//Sign_Up.html">
-                  <button className="btn1">Sign Up</button>
-                </Link>
-              </li>
-              <li className="link">
-               
-                <Link href="../Login/Login.html">
-                  <button className="btn1">Login</button>
-                </Link>
-              </li>
-        </ul>
-    </div>
+    <nav className="nav">
+         <Link to="/"> Stay Healthy Inc.</Link>
+          <ul>
+          <li><Link to ="/" > Home</Link></li>
+        <li><Link to ="/appointment" > Appointment</Link></li>
+        <li><Link to="/Signup"> Sign Up</Link></li>
+        <li><Link to="/Login"> Login</Link></li>
+     </ul>
+
+    </nav>
+     
+
   )
 }
 
